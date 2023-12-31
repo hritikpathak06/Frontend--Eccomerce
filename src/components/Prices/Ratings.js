1269b76bc4459b3784132f0a929e4c7b90120e34
Ritik Kumar Pathak <ritik.p@awesomesuite.com>
2023-12-31 17:36:49 +0530

    filter-section-added

---
+++ src/components/Prices/Ratings.js
@@ -0,0 +1,27 @@
+export const ratings = [
+  {
+    _id: 0,
+    name: "⭐",
+    array: [1, 1.5],
+  },
+  {
+    _id: 1,
+    name: "⭐⭐",
+    array: [1.5, 2],
+  },
+  {
+    _id: 2,
+    name: "⭐⭐⭐",
+    array: [2.5, 3],
+  },
+  {
+    _id: 3,
+    name: "⭐⭐⭐⭐",
+    array: [3, 3.5],
+  },
+  {
+    _id: 4,
+    name: "⭐⭐⭐⭐⭐",
+    array: [4, 5],
+  },
+];
